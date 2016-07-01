752eb94d09572fc3844ea638036c2df5ff46e9ff
Derek Li <lwy.capa@gmail.com>
2016-07-01 16:31:08 -0700

    Test Swagger

---
+++ Service/controllers/DefaultService.js
@@ -0,0 +1,29 @@
+'use strict';
+
+exports.rootGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  **/
+  // no response value expected for this operation
+  res.end();
+}
+
+exports.rootPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * name (String)
+  * year (String)
+  **/
+  // no response value expected for this operation
+  res.end();
+}
+
+exports.test_pathIdGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * id (String)
+  **/
+  // no response value expected for this operation
+  res.end();
+}
+
